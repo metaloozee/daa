1cdbe66c3bf66f7ed4fe20c23fbfae360b4b1849
metaloozee <ayanparkar123@gmail.com>
2025-04-23 18:30:46 +0530

    feat: better package management
    insertion sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from lib.merge_sort import merge_sort
+from sorting import insertion_sort, selection_sort, merge_sort, quick_sort
 
 A = [12, 3, 7, 9, 14, 6, 11, 2]
-print("Array before merge_sort: ", A)
-merge_sort(A, 0, len(A) - 1)
-print("Array after merge_sort: ", A)
+print("Array before sort: ", A)
+quick_sort(A, 0, len(A) - 1)
+print("Array after sort: ", A)
