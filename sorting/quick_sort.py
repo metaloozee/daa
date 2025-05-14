a8e74ca7ab64ef1575c9eaa356c73d5259f4e8b2
metaloozee <ayanparkar123@gmail.com>
2025-05-14 23:52:16 +0530

    refactor: quick sort

--- sorting/quick_sort.py
+++ sorting/quick_sort.py
@@ -1,15 +1,28 @@
-def quick_sort(A, p, r):
-    if p < r:
-        q = partition(A, p, r)
-        quick_sort(A, p, q - 1)
-        quick_sort(A, q + 1, r)
+def quick_sort(A, low, high):
+    if low < high:
+        pivot = partition(A, low, high)
+        
+        quick_sort(A, low, pivot - 1)
+        quick_sort(A, pivot + 1, high)
 
-def partition(A, p, r):
-    x = A[r]
-    i = p - 1
-    for j in range(p, r):
-        if A[j] <= x:
+def partition(A, low, high):
+    pivot = A[low]
+    i, j = low, high
+    
+    while i < j:
+        while A[i] <= pivot and i <= high - 1:
             i += 1
+        while A[j] >= pivot and j >= low + 1:
+            j -= 1
+        
+        if (i < j):
             A[i], A[j] = A[j], A[i]
-    A[i + 1], A[r] = A[r], A[i + 1]
-    return i + 1
+    
+    A[low], A[j] = A[j], A[low]
+    return j
+
+if __name__ == "__main__":
+    A = [30, 10, 50, 20, 40]
+    print(f"Array before quick sort: {A}")
+    quick_sort(A, 0, len(A) - 1)
+    print(f"Array after quick sort: {A}")
